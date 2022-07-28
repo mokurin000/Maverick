3a509493d77662dc947aeae65f42f33344945b0f
poly000 <1348292515@qq.com>
2022-07-29 01:10:50 +0800

    update | improve | fix

--- upload_to_poly.sh
+++ upload_to_poly.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+git add .
+git commit -m "update | improve | fix"
+git push
+
 python3 mvrk.py --config ./demo_src/config.py --source_dir ./demo_src/ --build_dir ./tmp/
 rsync -rP --delete tmp/* ../poly000.github.io
 cp google8812b0ec10b9d67f.html ../poly000.github.io
