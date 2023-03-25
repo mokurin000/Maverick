ecd0c7c82868d4c0f9088a9ad7cb7aca32eb3d48
poly000 <1348292515@qq.com>
2023-03-25 16:35:23 +0800

    update | improve | fix

--- upload_to_poly.sh
+++ upload_to_poly.sh
@@ -6,7 +6,7 @@ git push
 
 python3 mvrk.py --config ./demo_src/config.py --source_dir ./demo_src/ --build_dir ./tmp/
 
-sed -i 's!2023 poly000!2023 poly000 | <a href="https://icp.gov.moe/?keyword=20230513" target="_blank">萌ICP备20230513号</a>!' tmp/index.html
+sed -i 's! poly000</span>! poly000 | <a href="https://icp.gov.moe/?keyword=20230513" target="_blank">萌ICP备20230513号</a></span>!' tmp/index.html
 
 rsync -rP --delete tmp/* ../poly000.github.io
 cp google8812b0ec10b9d67f.html ../poly000.github.io
