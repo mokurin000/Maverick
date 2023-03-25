42bdb43a9bd4c032a1ac95ac56a0fe10c463321f
poly000 <1348292515@qq.com>
2023-03-25 16:27:09 +0800

    add 萌备案

--- upload_to_poly.sh
+++ upload_to_poly.sh
@@ -5,6 +5,9 @@ git commit -m "update | improve | fix"
 git push
 
 python3 mvrk.py --config ./demo_src/config.py --source_dir ./demo_src/ --build_dir ./tmp/
+
+sed -i "s/2023 poly000/2023 poly000 | <a href="https://icp.gov.moe/?keyword=20230513" target="_blank">萌ICP备20230513号</a>"tmp/index.html
+
 rsync -rP --delete tmp/* ../poly000.github.io
 cp google8812b0ec10b9d67f.html ../poly000.github.io
 (cd ../poly000.github.io && git add . && git commit -m update  && git push)
