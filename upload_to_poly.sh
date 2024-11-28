72f0237806f6598c86f021519b515a2a0a783c4f
poly000 <1348292515a@gmail.com>
2024-11-28 13:19:14 +0800

    update | improve | fix

--- upload_to_poly.sh
+++ upload_to_poly.sh
@@ -12,8 +12,8 @@ git push
 
 python mvrk.py --config ./demo_src/config.py --source_dir ./demo_src/ --build_dir ./tmp/
 
-sed -i 's! poly000</span>! poly000 | <a href="https://icp.gov.moe/?keyword=20230513" target="_blank">萌ICP备20230513号</a></span>!' tmp/index.html
+sed -i 's! poly000</span>! mokurin000 | <a href="https://icp.gov.moe/?keyword=20230513" target="_blank">萌ICP备20230513号</a></span>!' tmp/index.html
 
-rsync -rP --delete tmp/* ../poly000.github.io
-cp google8812b0ec10b9d67f.html ../poly000.github.io
+cp -r tmp/* ../mokurin000.github.io/
+cp google8812b0ec10b9d67f.html ../mokurin000.github.io/
 (cd ../poly000.github.io && git add . && git commit -m update  && git push)
