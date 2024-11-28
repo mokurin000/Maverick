784ec6c1b73b698fbc2baad1a4d7479cf11d3572
poly000 <1348292515a@gmail.com>
2024-11-28 13:17:27 +0800

    update | improve | fix

--- upload_to_poly.sh
+++ upload_to_poly.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source venv/bin/activate
+source .venv/*/activate
 
 if [ $? != 0 ]
 then exit 1
@@ -10,7 +10,7 @@ git add .
 git commit -m "update | improve | fix"
 git push
 
-python3 mvrk.py --config ./demo_src/config.py --source_dir ./demo_src/ --build_dir ./tmp/
+python mvrk.py --config ./demo_src/config.py --source_dir ./demo_src/ --build_dir ./tmp/
 
 sed -i 's! poly000</span>! poly000 | <a href="https://icp.gov.moe/?keyword=20230513" target="_blank">萌ICP备20230513号</a></span>!' tmp/index.html
 
